62769f90245221b9d2e07b46738222fc649d4d5f
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-12-25 12:37:31 +0100

    simplified configuration of job

--- jobs-config/config.js
+++ jobs-config/config.js
@@ -1,28 +1,33 @@
 const TASK_HARVESTING_COLLECTING = 'http://lblod.data.gift/id/job/concept/TaskType/collecting';
 const TASK_HARVESTING_IMPORTING = 'http://lblod.data.gift/id/job/concept/TaskType/importing';
 const TASK_HARVESTING_REPARING = 'http://lblod.data.gift/id/job/concept/TaskType/reparing';
-const TASK_HARVESTING_VALIDATING = 'http://lblod.data.gift/id/job/concept/TaskType/validating';
+//const TASK_HARVESTING_VALIDATING = 'http://lblod.data.gift/id/job/concept/TaskType/validating';
 const TASK_HARVESTING_MIRRORING = 'http://lblod.data.gift/id/job/concept/TaskType/mirroring';
+const JOB_LBLOD_HARVESTING = 'http://lblod.data.gift/id/job/concept/JobType/lblodHarvesting';
 
 //Some declarative boilerplate to manage the task order of the job. Will probably be prettier later
-export default {
-  'http://lblod.data.gift/id/job/concept/JobType/lblodHarvesting': {
-    knownTaskTypes: [ TASK_HARVESTING_COLLECTING, TASK_HARVESTING_IMPORTING, TASK_HARVESTING_REPARING, TASK_HARVESTING_VALIDATING, TASK_HARVESTING_MIRRORING ],
-    //key is current step, value is next step.
-    //Configuration is now linear, tree or graph like task configuration is possible in future, but will required code changes in the controller
-    tasksConfiguration: {
-      TASK_HARVESTING_COLLECTING : {
-        index: "0",
-        type: TASK_HARVESTING_IMPORTING
-      },
-      TASK_HARVESTING_IMPORTING : {
-        index: "1",
-        type: TASK_HARVESTING_REPARING
-      },
-      TASK_HARVESTING_REPARING: {
-        index: "2",
-        type: TASK_HARVESTING_MIRRORING
-      }
-    }
+//Configuration is now linear, tree or graph like task configuration is possible in future, but will required code changes in the controller
+const CONFIG = {};
+
+//config for lblod harvesting
+CONFIG[JOB_LBLOD_HARVESTING] = { };
+CONFIG[JOB_LBLOD_HARVESTING]['tasksConfiguration'] = [
+  {
+    currentTaskType: TASK_HARVESTING_COLLECTING,
+    nextTaskType: TASK_HARVESTING_IMPORTING,
+    nextIndex: '1'
+    //For complex configuration, a key dependsOn could be added
+  },
+  {
+    currentTaskType: TASK_HARVESTING_IMPORTING,
+    nextTaskType: TASK_HARVESTING_REPARING,
+    nextIndex: '2'
+  },
+  {
+    currentTaskType: TASK_HARVESTING_REPARING,
+    nextTaskType: TASK_HARVESTING_MIRRORING,
+    nextIndex: '3'
   }
-};
+];
+
+export default CONFIG;
