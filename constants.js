0a2214f268935628de06d0501a5c054106c59aad
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-12-25 11:53:14 +0100

    Update URI's

--- constants.js
+++ constants.js
@@ -1,16 +1,16 @@
-export const STATUS_BUSY = 'http://lblod.data.gift/vocabularies/processingJobs/status/busy';
-export const STATUS_SCHEDULED = 'http://lblod.data.gift/vocabularies/processingJobs/status/scheduled';
-export const STATUS_SUCCESS = 'http://lblod.data.gift/vocabularies/processingJobs/status/success';
-export const STATUS_FAILED = 'http://lblod.data.gift/vocabularies/processingJobs/status/failed';
+export const STATUS_BUSY = 'http://lblod.data.gift/id/job/concept/JobStatus/busy';
+export const STATUS_SCHEDULED = 'http://lblod.data.gift/id/job/concept/JobStatus/scheduled';
+export const STATUS_SUCCESS = 'http://lblod.data.gift/id/job/concept/JobStatus/success';
+export const STATUS_FAILED = 'http://lblod.data.gift/id/job/concept/JobStatus/failed';
 
 export const JOB_TYPE = 'http://vocab.deri.ie/cogs#Job';
-export const TASK_TYPE = 'http://lblod.data.gift/vocabularies/processingJobs/Task';
+export const TASK_TYPE = 'http://lblod.data.gift/vocabularies/Job/Task';
 export const ERROR_TYPE= 'http://open-services.net/ns/core#Error';
 
 export const PREFIXES = `
   PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-  PREFIX jobs: <http://lblod.data.gift/vocabularies/processingJobs/>
-  PREFIX terms: <http://purl.org/dc/terms/>
+  PREFIX lblodJob: <http://lblod.data.gift/vocabularies/Job/>
+  PREFIX dct: <http://purl.org/dc/terms/>
   PREFIX prov: <http://www.w3.org/ns/prov#>
   PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
   PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
@@ -18,5 +18,5 @@ export const PREFIXES = `
   PREFIX cogs: <http://vocab.deri.ie/cogs#>
 `;
 
-export const TASK_URI_PREFIX = 'http://lblod.data.gift/processingJobs/task/id/';
-export const ERROR_URI_PREFIX = 'http://lblod.data.gift/processingJobs/errors/id/';
+export const TASK_URI_PREFIX = 'http://lblod.data.gift/id/job/task/';
+export const ERROR_URI_PREFIX = 'http://lblod.data.gift/id/job/error/';
--- jobs-config/config.js
+++ jobs-config/config.js
@@ -1,12 +1,12 @@
-const TASK_HARVESTING_COLLECTING = 'http://lblod.data.gift/vocabularies/jobs/concept/TaskType/collecting';
-const TASK_HARVESTING_IMPORTING = 'http://lblod.data.gift/vocabularies/jobs/concept/TaskType/importing';
-const TASK_HARVESTING_REPARING = 'http://lblod.data.gift/vocabularies/jobs/concept/TaskType/reparing';
-const TASK_HARVESTING_VALIDATING = 'http://lblod.data.gift/vocabularies/jobs/concept/TaskType/validating';
-const TASK_HARVESTING_MIRRORING = 'http://lblod.data.gift/vocabularies/jobs/concept/TaskType/mirroring';
+const TASK_HARVESTING_COLLECTING = 'http://lblod.data.gift/id/job/concept/TaskType/collecting';
+const TASK_HARVESTING_IMPORTING = 'http://lblod.data.gift/id/job/concept/TaskType/importing';
+const TASK_HARVESTING_REPARING = 'http://lblod.data.gift/id/job/concept/TaskType/reparing';
+const TASK_HARVESTING_VALIDATING = 'http://lblod.data.gift/id/job/concept/TaskType/validating';
+const TASK_HARVESTING_MIRRORING = 'http://lblod.data.gift/id/job/concept/TaskType/mirroring';
 
 //Some declarative boilerplate to manage the task order of the job. Will probably be prettier later
 export default {
-  'http://lblod.data.gift/vocabularies/jobs/concept/JobType/lblodHarvesting': {
+  'http://lblod.data.gift/id/job/concept/JobType/lblodHarvesting': {
     knownTaskTypes: [ TASK_HARVESTING_COLLECTING, TASK_HARVESTING_IMPORTING, TASK_HARVESTING_REPARING, TASK_HARVESTING_VALIDATING, TASK_HARVESTING_MIRRORING ],
     //key is current step, value is next step.
     //Configuration is now linear, tree or graph like task configuration is possible in future, but will required code changes in the controller
