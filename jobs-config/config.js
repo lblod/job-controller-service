dd5358fad01ddd6c6e33f53588590622113eed52
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2021-02-26 16:33:02 +0100

    added extra job

--- jobs-config/config.js
+++ jobs-config/config.js
@@ -6,6 +6,8 @@ const TASK_HARVESTING_MIRRORING = 'http://lblod.data.gift/id/jobs/concept/TaskOp
 const JOB_LBLOD_IMPORT = 'http://lblod.data.gift/id/jobs/concept/JobOperation/lblodImportCentraleVindplaats';
 const TASK_IMPORTING_CENTRALE_VINDPLAATS = 'http://lblod.data.gift/id/jobs/concept/TaskOperation/importCentraleVindplaats';
 
+const JOB_LBLOD_HARVEST_AND_IMPORT = 'http://lblod.data.gift/id/jobs/concept/JobOperation/lblodHarvestAndImportCentraleVindplaats';
+
 //Some declarative boilerplate to manage the task order of the job. Will probably be prettier later
 //Configuration is now linear, tree or graph like task configuration is possible in future, but will required code changes in the controller
 const CONFIG = {};
@@ -29,6 +31,32 @@ CONFIG[JOB_LBLOD_HARVESTING]['tasksConfiguration'] = [
     currentOperation: TASK_HARVESTING_IMPORTING,
     nextOperation: TASK_HARVESTING_MIRRORING,
     nextIndex: '2'
+  }
+];
+
+CONFIG[JOB_LBLOD_HARVEST_AND_IMPORT] = { };
+CONFIG[JOB_LBLOD_HARVEST_AND_IMPORT]['tasksConfiguration'] = [
+  {
+    currentOperation: null, //The first step is merely here for documentation purposes
+    nextOperation: TASK_HARVESTING_IMPORTING,
+    nextIndex: '0'
+    //For complex configuration, a key dependsOn could be added
+  },
+  {
+    currentOperation: TASK_HARVESTING_COLLECTING,
+    nextOperation: TASK_HARVESTING_IMPORTING,
+    nextIndex: '1'
+    //For complex configuration, a key dependsOn could be added
+  },
+  {
+    currentOperation: TASK_HARVESTING_IMPORTING,
+    nextOperation: TASK_HARVESTING_MIRRORING,
+    nextIndex: '2'
+  },
+  {
+    currentOperation: TASK_HARVESTING_MIRRORING,
+    nextOperation: TASK_IMPORTING_CENTRALE_VINDPLAATS,
+    nextIndex: '3'
   }
 ];
 
