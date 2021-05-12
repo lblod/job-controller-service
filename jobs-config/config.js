9498924ac8e6aa2384044e976e3381927915ded9
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2021-05-12 09:50:04 +0200

    [WIP] adding jobs

--- jobs-config/config.js
+++ jobs-config/config.js
@@ -1,3 +1,10 @@
+//Some declarative boilerplate to manage the task order of the job. Will probably be prettier later
+//Configuration is now linear, tree or graph like task configuration is possible in future, but will required code changes in the controller
+const CONFIG = {};
+
+/**
+ * HARVESTING
+ **/
 const JOB_LBLOD_HARVESTING = 'http://lblod.data.gift/id/jobs/concept/JobOperation/lblodHarvesting';
 const TASK_HARVESTING_COLLECTING = 'http://lblod.data.gift/id/jobs/concept/TaskOperation/collecting';
 const TASK_HARVESTING_IMPORTING = 'http://lblod.data.gift/id/jobs/concept/TaskOperation/importing';
@@ -9,10 +16,6 @@ const JOB_LBLOD_IMPORT = 'http://lblod.data.gift/id/jobs/concept/JobOperation/lb
 const TASK_IMPORTING_CENTRALE_VINDPLAATS = 'http://lblod.data.gift/id/jobs/concept/TaskOperation/importCentraleVindplaats';
 
 const JOB_LBLOD_HARVEST_AND_IMPORT = 'http://lblod.data.gift/id/jobs/concept/JobOperation/lblodHarvestAndImportCentraleVindplaats';
-
-//Some declarative boilerplate to manage the task order of the job. Will probably be prettier later
-//Configuration is now linear, tree or graph like task configuration is possible in future, but will required code changes in the controller
-const CONFIG = {};
 
 //config for lblod harvesting
 CONFIG[JOB_LBLOD_HARVESTING] = { };
@@ -90,5 +93,40 @@ CONFIG[JOB_LBLOD_IMPORT]['tasksConfiguration'] = [
     nextIndex: '0'
   }
 ] ;
+
+/**
+ * delta-initial-cache-graph-sync-job
+ **/
+
+const JOB_LBLOD_INITIAL_CACHE_SYNC_JOB_OPERATION_LEIDINGGEVENDEN = 'http://redpencil.data.gift/id/jobs/concept/JobOperation/deltas/initibalCacheGraphSyncing/leidinggevenden';
+const TASK_INITIAL_CACHE_SYNC = 'http://redpencil.data.gift/id/jobs/concept/TaskOperation/deltas/initialCacheGraphSyncing';
+
+CONFIG[JOB_LBLOD_INITIAL_CACHE_SYNC_JOB_OPERATION_LEIDINGGEVENDEN] = { };
+CONFIG[JOB_LBLOD_INITIAL_CACHE_SYNC_JOB_OPERATION_LEIDINGGEVENDEN]['tasksConfiguration'] = [
+  {
+    currentOperation: null, //The first step is merely here for documentation purposes
+    nextOperation: TASK_INITIAL_CACHE_SYNC,
+    nextIndex: '0'
+  }
+  // {
+  //   currentOperation: TASK_DELTA_HEALING_DIFF_CALCULATION,
+  //   nextOperation: TASK_DELTA_HEALING_PATCH,
+  //   nextIndex: '1'
+  // }
+];
+
+
+const TASK_DUMP_FILE_CREATION = 'http://redpencil.data.gift/id/jobs/concept/TaskOperation/deltas/deltaDumpFileCreation';
+//JOB: operation (this should be conifugrable in the service)
+const JOB_LBLOD_DELTA_DUMP_FILE_CREATION_JOB_LEIDINGGEVENDEN = 'http://redpencil.data.gift/id/jobs/concept/JobOperation/deltas/deltaDumpFileCreation/leidinggevenden';
+CONFIG[JOB_LBLOD_DELTA_DUMP_FILE_CREATION_JOB_LEIDINGGEVENDEN] = { };
+CONFIG[JOB_LBLOD_DELTA_DUMP_FILE_CREATION_JOB_LEIDINGGEVENDEN]['tasksConfiguration'] = [
+  {
+    currentOperation: null, //The first step is merely here for documentation purposes
+    nextOperation: TASK_DUMP_FILE_CREATION,
+    nextIndex: '0'
+  }
+];
+
 
 export default CONFIG;
