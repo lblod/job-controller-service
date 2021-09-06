d0f60e3ad758ba085d05cb8829ae9cf381e6a1a0
Unknown <achrafatauil@hotmail.com>
2021-09-06 13:22:39 +0200

    add scheduleJobs logic & constants

--- constants.js
+++ constants.js
@@ -2,10 +2,12 @@ export const STATUS_BUSY = 'http://redpencil.data.gift/id/concept/JobStatus/busy
 export const STATUS_SCHEDULED = 'http://redpencil.data.gift/id/concept/JobStatus/scheduled';
 export const STATUS_SUCCESS = 'http://redpencil.data.gift/id/concept/JobStatus/success';
 export const STATUS_FAILED = 'http://redpencil.data.gift/id/concept/JobStatus/failed';
+export const STATUS_READY_TO_BE_CACHED = 'http://lblod.data.gift/file-download-statuses/ready-to-be-cached';
 
 export const JOB_TYPE = 'http://vocab.deri.ie/cogs#Job';
 export const TASK_TYPE = 'http://redpencil.data.gift/vocabularies/tasks/Task';
 export const ERROR_TYPE= 'http://open-services.net/ns/core#Error';
+export const REQUEST_HEADER = 'http://data.lblod.info/request-headers/accept/text/html';
 
 export const PREFIXES = `
   PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
@@ -18,7 +20,14 @@ export const PREFIXES = `
   PREFIX cogs: <http://vocab.deri.ie/cogs#>
   PREFIX adms: <http://www.w3.org/ns/adms#>
   PREFIX schema: <http://schema.org/>
+  PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+  PREFIX hrvst: <http://lblod.data.gift/vocabularies/harvesting/>
+  PREFIX rpioHttp: <http://redpencil.data.gift/vocabularies/http/>
 `;
 
 export const TASK_URI_PREFIX = 'http://redpencil.data.gift/id/task/';
 export const ERROR_URI_PREFIX = 'http://redpencil.data.gift/id/jobs/error/';
+
+export const INPUTCONTAINER_URI_PREFIX = 'http://redpencil.data.gift/id/jobs/error/';
+export const HARVESTINGCOLLECTION_URI_PREFIX = 'http://lblod.data.gift/vocabularies/harvesting/';
+export const REMOTEDATAOBJECT_URI_PREFIX = 'http://redpencil.data.gift/id/jobs/error/';
